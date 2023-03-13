cb3bf89b6de07354f3942a001dfd0b81fa525600
Jeanne Allen <tebazele@gmail.com>
2023-03-13 15:13:59 -0600

    templates written in model

--- app/Controllers/QuestionsController.js
+++ app/Controllers/QuestionsController.js
@@ -1,6 +1,10 @@
 import { questionsService } from '../Services/QuestionsService.js';
 import { Pop } from '../Utils/Pop.js'
 
+function _drawQuestion() {
+
+}
+
 export class QuestionsController {
     constructor() {
         console.log('hello from the questions controller!');
